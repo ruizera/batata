cfc51ac3d67ed3951cdaa0c31cd167651a41d03a
RUIZVIC <victor.leopoldino@itau-unibanco.com.br>
2019-10-01 19:09:00 -0300

    abc

--- _javascript/funcoes.js
+++ _javascript/funcoes.js
@@ -2,3 +2,35 @@ function mudaFoto (foto) {
 		 document.getElementById("icone").src= foto;
 	 
 	 }
+	 
+
+	 isLoged = localStorage.getItem("logaed");
+function redirectPage(){
+		loged();
+		window.location = "index.html";
+
+		return false;
+}
+function loged(){
+			 
+		isLoged = true;
+		localStorage.setItem("logaed", isLoged);
+	 }
+
+	function irLogin(){
+		if(!isLoged){
+			document.getElementById("lnkLogin").href="cadastro.html";		
+		}else{
+			document.getElementById("lnkLogin").href="#";					
+		}
+	}
+	 
+	 function cadastrar(){
+	
+	 if(!isLoged){
+			document.getElementById("login").innerHTML="Cadastre-se";
+
+		}else{
+			document.getElementById("login").innerHTML="Logado ";
+		}
+}
